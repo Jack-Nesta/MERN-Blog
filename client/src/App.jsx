556ec76d7f380e87bf3de07591fca9f1534bebaf
Patrick Kariuki <pakariuki@gmail.com>
2024-07-29 18:14:53 +0300

    Updated the routing pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects.jsx";
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>App</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/About" element={<About />} />
+        <Route path="/Dashboard" element={<Dashboard />}></Route>
+        <Route path="/SignIn" element={<SignIn />}></Route>
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="Projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <h1 className='text-red-500'>About</h1>
+  )
+}
